d51d0ef09ba412b77c83a542c1865e22e9418f40
weijunext <weijunext@gmail.com>
2025-07-23 10:22:18 +0800

    style: enhance header link styles for improved readability and consistency

--- components/header/HeaderLinks.tsx
+++ components/header/HeaderLinks.tsx
@@ -13,7 +13,7 @@ const HeaderLinks = () => {
   const headerLinks: HeaderLink[] = tHeader.raw("links");
 
   return (
-    <div className="hidden md:flex flex-row items-center gap-x-2 text-sm text-muted-foreground">
+    <div className="hidden md:flex flex-row items-center gap-x-2 text-sm font-medium text-muted-500">
       {headerLinks.map((link) => (
         <I18nLink
           key={link.name}
@@ -24,7 +24,7 @@ const HeaderLinks = () => {
           rel={link.rel || undefined}
           className={cn(
             "rounded-xl px-4 py-2 flex items-center gap-x-1 hover:bg-accent-foreground/10 hover:text-accent-foreground",
-            pathname === link.href && "font-medium text-accent-foreground"
+            pathname === link.href && "font-semibold text-accent-foreground"
           )}
         >
           {link.name}
